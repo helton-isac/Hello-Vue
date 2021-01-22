9c16dbce7b0f9aa9356a528f99e5abe35321c1ee
Helton Isac <helton.isac@gmail.com>
2021-01-22 10:51:12 -0300

    Changing Background color, msg and font-color

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Hello Vue.js - by Helton Isac"/>
   </div>
 </template>
 
@@ -24,5 +24,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: #2BD1FF;
 }
 </style>
